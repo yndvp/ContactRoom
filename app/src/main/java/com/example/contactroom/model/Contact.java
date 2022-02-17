f09f96ebe808ae297bbd5f5dcdbfaf02473f1667
yndvp <58580623+yndvp@users.noreply.github.com>
2022-02-16 21:02:10 -0500

    Set up ROOM library dependencies & Set up model class and entity

---
+++ app/src/main/java/com/example/contactroom/model/Contact.java
@@ -0,0 +1,41 @@
+package com.example.contactroom.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contact_table")
+public class Contact {
+
+    @PrimaryKey(autoGenerate = true)
+//    @NonNull -> redundant(as it's implied)
+//    @ColumnInfo(name = "id") -> Column name not necessary since it's the same as the filed name
+    private int id;
+
+//    @ColumnInfo(name = "name")
+    private String name;
+
+//    @ColumnInfo(name = "occupation")
+    private String occupation;
+
+    public Contact() {
+    }
+
+    public Contact(@NonNull String name, @NonNull String occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+}
