f6898add0ff09a201ec0e38cfd4460cf0ede32bc
yndvp <58580623+yndvp@users.noreply.github.com>
2022-02-16 21:17:52 -0500

    Set up android ROOM DAO & LiveData

---
+++ app/src/main/java/com/example/contactroom/data/ContactDao.java
@@ -0,0 +1,24 @@
+package com.example.contactroom.data;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.contactroom.model.Contact;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    // CRUD
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Contact contact);
+
+    @Query("DELETE FROM contact_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM contact_table ORDER BY name ASC")
+    LiveData<List<Contact>> getAllContacts();
+}
